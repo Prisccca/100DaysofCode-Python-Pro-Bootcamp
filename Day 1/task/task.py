926f68afd990dabfb2e3bd1ef7b0e2fa643973fb
Priscilla Falcão <priscillaffd@gmail.com>
2024-08-19 23:38:30 -0300

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("This is the program: Band Name Generator!!!")
+city_name = input("What is your city name? \n")
+pet_name = input("What is your pet name? \n")
+print("Your band name is: " + city_name + " " + pet_name)
